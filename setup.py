49e12a0a30e4af7c5e0156d1521886590f77c3f1
obinnaeye <obinnaeye@yahoo.com>
2018-02-17 10:21:15 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Foo',
+      version='0.0.1',
+      description='Python Distribution Utilities',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+     )
